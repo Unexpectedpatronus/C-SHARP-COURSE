947aec987cbd5a2f4f61617cbf0f80dbea1de17b
Evgeny Odintsov <ev1genial@gmail.com>
2024-06-05 19:49:57 +0300

    First

---
+++ HW-01/Program.cs
@@ -0,0 +1,21 @@
+﻿bool restart = false;
+
+do
+{
+    Console.WriteLine("Please, enter a message.");
+    string? message = null;
+    DateTime startedAt = DateTime.Now;
+    
+    do
+    {
+        message = Console.ReadLine();
+    } while (string.IsNullOrWhiteSpace(message));
+
+    DateTime stoppedAt = DateTime.Now;
+    TimeSpan span = stoppedAt - startedAt;
+    Console.WriteLine($"Time for typing was {span}\nIf you want to continue, press 1, else press enter");
+    
+    string? choice = Console.ReadLine();
+    
+    restart = choice == "1";
+} while (restart);
