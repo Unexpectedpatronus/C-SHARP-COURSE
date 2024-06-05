0b72a1a015da0eba85ba6b1560f46f769fccf3ea
Evgeny Odintsov <ev1genial@gmail.com>
2024-06-05 22:19:45 +0300

    Added random generator

--- HW-01/Program.cs
+++ HW-01/Program.cs
@@ -1,21 +1,38 @@
 ﻿bool restart = false;
+bool useTestData = false;
+
+string[] options = new[]
+{
+    "This is the first string",
+    "This is the second string",
+    "This is the third string",
+    "This is the fourth string"
+};
 
 do
 {
-    Console.WriteLine("Please, enter a message.");
     string? message = null;
-    DateTime startedAt = DateTime.Now;
-    
-    do
-    {
-        message = Console.ReadLine();
-    } while (string.IsNullOrWhiteSpace(message));
 
-    DateTime stoppedAt = DateTime.Now;
-    TimeSpan span = stoppedAt - startedAt;
-    Console.WriteLine($"Time for typing was {span}\nIf you want to continue, press 1, else press enter");
-    
+    DateTime startedAt = DateTime.Now;
+    if (useTestData == false)
+    {
+        Console.WriteLine("Please, enter a message.");
+        do
+        {
+            message = Console.ReadLine();
+        } while (string.IsNullOrWhiteSpace(message));
+    }
+    else
+    {
+        Random rnd = new Random();
+        message = options[rnd.Next(0, options.Length)];
+    }
+
+    TimeSpan span = DateTime.Now - startedAt;
+    Console.WriteLine($"Time for typing message \"{message}\" was {span} seconds");
+    Console.WriteLine("If you want to continue, press 1, else press enter");
+
     string? choice = Console.ReadLine();
-    
+
     restart = choice == "1";
 } while (restart);
