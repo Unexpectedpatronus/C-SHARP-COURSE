0dba9899e221d174beb84c2fd90477746c431ce8
Evgeny Odintsov <ev1genial@gmail.com>
2024-06-11 21:09:13 +0300

    Integration

---
+++ HW-02-numeric-analysis/HW-02-numeric-analysis/IntegralCalculus.cs
@@ -0,0 +1,48 @@
+﻿namespace HW_02_numeric_analysis;
+
+public class IntegralCalculus
+{
+    public static double Calculate(Func<double, double> func, double x1, double x2, double precision)
+    {
+        if (x1 >= x2)
+        {
+            throw new NotImplementedException("Invalid input: 'x1' must be less than 'x2'.");
+        }
+
+        int amountOfParts = 1;
+        double previousResult = Integrate(func, x1, x2, amountOfParts);
+        double currentResult;
+        do
+        {
+            amountOfParts *= 2;
+            currentResult = Integrate(func, x1, x2, amountOfParts);
+
+            if (Math.Abs(currentResult - previousResult) < precision)
+            {
+                break;
+            }
+
+            previousResult = currentResult;
+        } while (true);
+
+        return currentResult;
+    }
+
+    private static double Integrate(Func<double, double> func, double x1, double x2, int amountOfParts)
+    {
+        double result = 0;
+        double stepSize = (x2 - x1) / amountOfParts;
+
+        for (int i = 0; i < amountOfParts; ++i)
+        {
+            double xStart = x1 + stepSize * i;
+            double xEnd = xStart + stepSize;
+            double yStart = func(xStart);
+            double yEnd = func(xEnd);
+
+            result += (yStart + yEnd) / 2 * stepSize;
+        }
+
+        return result;
+    }
+}
---
+++ HW-02-numeric-analysis/HW-02-numeric-analysis/Program.cs
@@ -0,0 +1,13 @@
+﻿using HW_02_numeric_analysis;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+Func<double, double> func = Math.Sin;
+double x1 = 0;
+double x2 = Math.PI;
+double precision = 0.0001;
+
+double result = IntegralCalculus.Calculate(func, x1, x2, precision);
+Console.WriteLine($"Приблизительная площадь криволинейной трапеции составляет: {result}");
+
